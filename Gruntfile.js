49a1dcb17e961a00392cc7ca6d12dc3b00becf88
Christopher Van <cvan@mozilla.com>
2014-02-02 16:16:23 -0800

    run grunt server + watch simultaneously

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,28 @@
 /*global module:false*/
 module.exports = function(grunt) {
+  var rewriteRulesSnippet = require('grunt-connect-rewrite/lib/utils').rewriteRequest;
+
+  var serverOptions = {
+    base: 'src',
+    debug: !!(process.env.DEBUG || process.env.DEBUG),
+    hostname: '0.0.0.0',
+    open: !!(process.env.OPEN || process.env.open),
+    port: process.env.PORT || process.env.port || 9000
+  };
+
+  var developmentOptions = {};
+  Object.keys(serverOptions).forEach(function(k) {
+    developmentOptions[k] = serverOptions[k];
+  });
+  developmentOptions.middleware = function(connect, options) {
+    return [
+      // RewriteRules support
+      rewriteRulesSnippet,
+      // Mount filesystem
+      connect.static(require('path').resolve(options.base))
+    ];
+  };
+
   grunt.initConfig({
     connect: {
       server: {
@@ -54,5 +77,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-nunjucks');
+  grunt.loadTasks('./node_modules/grunt-nunjucks/tasks');
+
+  grunt.registerTask('server', ['configureRewriteRules', 'connect:development']);
+  grunt.registerTask('default', ['server', 'watch:nunjucks']);
 };
--- README.md
+++ README.md
@@ -8,6 +8,9 @@ It's a list of mobile apps.
 We use [grunt](http://gruntjs.com/):
 
     npm install grunt-cli -g
+
+Then install our dependencies:
+
     npm install
 
 
@@ -16,8 +19,12 @@ We use [grunt](http://gruntjs.com/):
 Load from a page with an origin (i.e., a server). If you're running locally,
 use grunt to fire up a local server:
 
-    DEBUG=1 OPEN=1 grunt connect
+    DEBUG=1 OPEN=1 grunt server
 
 And to automatically recompile templates upon file changes:
 
     grunt watch
+
+To run both simultaneously:
+
+    DEBUG=1 OPEN=1 grunt
